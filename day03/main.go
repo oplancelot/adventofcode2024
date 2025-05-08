c8189f20a974d355592195cb0ec414d18a1640a0
lance <oplancelot@gmail.com>
2025-05-08 03:52:18 +0000

    day03 part2

--- day03/main.go
+++ day03/main.go
@@ -7,14 +7,21 @@ import (
 	"strconv"
 )
 
+// removeDoDontSections 删除 do() 和 don't() 之间的内容，以及从 don't() 到文件结束的内容
+// 删除 don't() 到下一个 do() 之间的内容（包含 don't 和 do）
+func removeDontDoSections(data string) string {
+	re := regexp.MustCompile(`(?s)don't\(\).*?(do\(\)|\z)`)
+	return re.ReplaceAllString(data, "")
+}
 func readInputAndSumMul(filename string) (int, error) {
 	data, err := os.ReadFile(filename)
 	if err != nil {
 		return 0, err
 	}
-
+	// 先删除 do() 和 don't() 之间的内容，以及从 don't() 到文件结束的内容
+	cleanData := removeDontDoSections(string(data))
 	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
-	matches := re.FindAllStringSubmatch(string(data), -1)
+	matches := re.FindAllStringSubmatch(string(cleanData), -1)
 	sum := 0
 	for _, match := range matches {
 		x, _ := strconv.Atoi(match[1])
